08b84c83810a6888492fc92c5bc08b3c240aeb1f
Tristen Nollman <46800978+NerdySouth@users.noreply.github.com>
2023-11-26 19:30:01 -0800

    Add Arch specific env variables to fish config for SSL dirs

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    #
+
+    # Arch specific env variables so cargo/rustc can find openssl
     export OPENSSL_DIR=/etc/ssl
+    export OPENSSL_INCLUDE_DIR=/usr/include/openssl
+    export OPENSSL_LIB_DIR=/usr/lib
     #display sys info 
     sleep 0.1 && macchina
 end
